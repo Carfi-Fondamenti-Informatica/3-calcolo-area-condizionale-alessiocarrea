9ab61720c0a2e56113c264ee5a21ed5d15a15923
alessiocarrea <118049896+alessiocarrea@users.noreply.github.com>
2022-11-21 11:15:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@ int main() {
    cin >> a >> b >> opzione;
    switch (opzione) {
       case 0:
-         cout << (a*b)/2;
+         cout << (a*b)/2 << endl;
          break;
       case 1:
-         cout << a*a;
+         cout << a*a << endl;
          break;
       case 2:
-         cout << a*b;
+         cout << a*b << endl;
          break;
       default:
          cout << "Opzione non valida";
