589d6ae843e93c593570efa7cdf9fe7fc07271e8
alessiocarrea <118049896+alessiocarrea@users.noreply.github.com>
2022-11-21 11:10:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+   float a, b;
+   int opzione;
+   cin >> a >> b >> opzione;
+   switch (opzione) {
+      case 0:
+         cout << (a*b)/2;
+         break;
+      case 1:
+         cout << a*a;
+         break;
+      case 2:
+         cout << a*b;
+         break;
+      default:
+         cout << "Opzione non valida";
+   }
    return 0;
 }
